2a465ba9ec8baac20cc53408571de40502c732a9
riefolog-cyber <riefologiovanni-netizen@users.noreply.github.com>
2026-05-17 21:22:38 +0200

    3file

--- js/app.js
+++ js/app.js
@@ -683,19 +683,6 @@ function App(){
     });
     var u3=fbClassiListen(function(lista){setClassiCustom(lista)});
     var u4=fbFavListen(user.uid,function(ids){setPreferiti(ids);});
-<<<<<<< HEAD
-    // Messaggi privati
-    var u7=msgPrivatiListen(user.role==="prof",myName(user),function(arr){
-      if(user.role==="prof"){
-        var m={};arr.forEach(function(msg){
-          if(!m[msg.cardId])m[msg.cardId]={};
-          m[msg.cardId][msg.studente]=msg;
-        });setMsgPrivatiMap(m);
-      } else {setMsgPrivatiStudente(arr);}
-    });
-=======
-  
->>>>>>> 207cf4b78ea40cc3047e2ff19c2e568d4907b348
     var u5=null;
     if(user.role==="studente"){
       u5=db.collection("ammonizioni").doc(myName(user)).onSnapshot(function(doc){
